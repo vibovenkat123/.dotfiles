e135384dd656eb55b9ba94bc6a3a31c81aea46ae
vibovenkat123 <vibovenkat@gmail.com>
2022-11-22 23:19:48 -0600

    update dependencies

--- installDependencies.sh
+++ installDependencies.sh
@@ -3,4 +3,5 @@ curl -sS https://starship.rs/install.sh | sh
 sudo apt install neovim exa pyenv tmux
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 
