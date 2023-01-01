f183a0f1637bcf13111c41919fcd65bcaa8a5a34
vibovenkat123 <vibovenkat@gmail.com>
2022-12-31 21:37:44 -0600

    only add readme to main branch

--- fish/config.sh
+++
@@ -1,5 +0,0 @@
-function mkcd --argument dir
-	mkdir $dir
-	cd $dir
-end
-	
--- installDependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-curl -sS https://starship.rs/install.sh | sh
-sudo apt install neovim exa pyenv tmux
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-
