fd525e0353826177774e407ee84a1a8245dccd7f
vibovenkat123 <vibovenkat@gmail.com>
2023-01-07 18:55:13 -0600

    fix: bugs

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,10 +1,10 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+local vaibhav_group  = vim.api.nvim_create_augroup("vaibhav_group", {})
 
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("BufWinEnter", {
-    group = ThePrimeagen_Fugitive,
+    group = vaibhav_group,
     pattern = "*",
     callback = function()
         if vim.bo.ft ~= "fugitive" then
@@ -17,13 +17,10 @@ autocmd("BufWinEnter", {
             vim.cmd.Git('push')
         end, opts)
 
-        -- rebase always
         vim.keymap.set("n", "<leader>P", function()
             vim.cmd.Git({'pull',  '--rebase'})
         end, opts)
 
-        -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-        -- needed if i did not set the branch up correctly
         vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
     end,
 })
