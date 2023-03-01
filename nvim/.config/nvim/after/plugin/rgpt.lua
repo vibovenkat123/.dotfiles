bd323259e2195f55273f4a0f50b005b5bf58bd9f
vibovenkat123 <vibovenkat@gmail.com>
2023-02-28 19:04:59 -0600

    feat: add the setup command for review-gpt

---
+++ nvim/.config/nvim/after/plugin/rgpt.lua
@@ -0,0 +1,9 @@
+require("rgpt").setup({
+    model = "text-davinci-003",
+    max_tokens = 200,
+    temperature = 0.2,
+    top_p = 1,
+    frequence_penalty = 1.2,
+    presence_penalty = 0.3,
+    best_of = 1,
+})
