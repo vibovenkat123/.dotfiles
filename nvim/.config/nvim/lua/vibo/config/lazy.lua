d5eadb84035d254ea93a329d03da91b5828bc32f
Vaibhav Venkat <vibovenkat@gmail.com>
2023-03-10 19:28:00 -0600

    feat: remove noice

--- nvim/.config/nvim/lua/vibo/config/lazy.lua
+++ nvim/.config/nvim/lua/vibo/config/lazy.lua
@@ -17,37 +17,6 @@ return require('lazy').setup({
         priority=1000,
     },
     {
-        "folke/noice.nvim",
-        config = function() 
-            require("noice").setup({
-                lsp = {
-                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                    override = {
-                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                        ["vim.lsp.util.stylize_markdown"] = true,
-                        ["cmp.entry.get_documentation"] = true,
-                    },
-                },
-                -- you can enable a preset for easier configuration
-                presets = {
-                    bottom_search = true, -- use a classic bottom cmdline for search
-                    command_palette = true, -- position the cmdline and popupmenu together
-                    long_message_to_split = true, -- long messages will be sent to a split
-                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                    lsp_doc_border = false, -- add a border to hover docs and signature help
-                },
-            })
-        end,
-        dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-            "MunifTanjim/nui.nvim",
-            -- OPTIONAL:
-            --   `nvim-notify` is only needed, if you want to use the notification view.
-            --   If not available, we use `mini` as the fallback
-            "rcarriga/nvim-notify",
-        }
-    },
-    {
         "simrat39/rust-tools.nvim",
         ft = "rs",
         event = "VeryLazy"
@@ -136,7 +105,7 @@ return require('lazy').setup({
 },
 {
     'nvim-treesitter/nvim-treesitter', build = ':TSUpdate',
-    config = function()
+    config = function() 
         require("vibo.plugins.treesitter")
     end
 },
