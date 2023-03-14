d6228eff5badc99d6ffece3aa073e2862468ccb0
Vaibhav Venkat <vibovenkat@gmail.com>
2023-03-14 15:46:18 -0500

    feat: switch to astronvim

--- nvim/.config/nvim/init.lua
+++
@@ -1,3 +0,0 @@
-require("vibo.config.options")
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
--- nvim/.config/nvim/lua/vibo/config/options/init.lua
+++
@@ -1,4 +0,0 @@
-require("vibo.config.options.lazy")
-require("vibo.config.options.lsp")
-require("vibo.config.options.settings")
-require("vibo.config.options.maps")
