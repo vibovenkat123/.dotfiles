739c6f0e5b10a7748f881e5780d3acbcbea91af0
Vaibhav Venkat <vibovenkat@gmail.com>
2023-03-23 20:30:57 -0500

    feat: make everforest good

--- nvim/.config/nvim/lua/vibo/config/lazy.lua
+++ nvim/.config/nvim/lua/vibo/config/lazy.lua
@@ -45,6 +45,8 @@ require('lazy').setup({
         'sainnhe/everforest',
         priority = 1000,
         config = function()
+            vim.g.everforest_background = 'hard'
+            vim.g.everforest_transparent_background = 1
             vim.cmd.colorscheme("everforest")
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
