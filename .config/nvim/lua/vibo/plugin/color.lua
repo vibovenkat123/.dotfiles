e13a802465746ad44401bd15d209c4a0d3da0320
Vaibhav Venkat <vibovenkat@gmail.com>
2023-04-27 18:16:25 -0500

    feat: add light modes

--- .config/nvim/lua/vibo/plugin/color.lua
+++ .config/nvim/lua/vibo/plugin/color.lua
@@ -2,12 +2,17 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   setup = {
-    flavor = "macchiato",
+    transparent_background = true,
+    background = {
+      light = "latte",
+      dark = "macchiato",
+    },
   },
   priority = 1000,
   config = function()
+    vim.opt.background = "dark"
     vim.cmd.colorscheme("catppuccin")
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   end
 }
